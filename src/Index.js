90a7f2971617898f8a4531b746d2d9bf9f3b1014
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-03-11 21:18:00 +0600

    db connection added

--- src/Index.js
+++ src/Index.js
@@ -1,10 +1,15 @@
 import {app} from "./app.js"
+import 'dotenv/config'
+import connectDB from "./db/connection.js"
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
